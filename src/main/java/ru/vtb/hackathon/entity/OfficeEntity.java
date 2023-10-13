a70b61cf79be38a6626c59abfccdaf3fa3352f38
Sedo-KFM <fedor122448@gmail.com>
2023-10-14 01:55:56 +0400

    OfficeEntity create

---
+++ src/main/java/ru/vtb/hackathon/entity/OfficeEntity.java
@@ -0,0 +1,23 @@
+package ru.vtb.hackathon.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.vtb.hackathon.model.OfficeTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "office")
+@NoArgsConstructor
+public class OfficeEntity {
+
+    @Id
+    private Long id;
+    private String salePointName;
+    private String addressString;
+    private String status;
+}
