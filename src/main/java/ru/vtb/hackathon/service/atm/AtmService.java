47ea34d6b3b03e00d5531181c04b1cc023848293
Sedo-KFM <fedor122448@gmail.com>
2023-10-14 16:19:05 +0400

    Filter by feature

--- src/main/java/ru/vtb/hackathon/service/atm/AtmService.java
+++ src/main/java/ru/vtb/hackathon/service/atm/AtmService.java
@@ -1,11 +1,16 @@
 package ru.vtb.hackathon.service.atm;
 
 import ru.vtb.hackathon.entity.AtmEntity;
+import ru.vtb.hackathon.model.dto.feature.Feature;
+import ru.vtb.hackathon.model.dto.feature.FeatureState;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AtmService {
 
     List<AtmEntity> findAllAtmAround(Double latitude, Double longitude, Double radius);
+
+    List<AtmEntity> FilterByFeatures(List<AtmEntity> atms, Map<Feature, Boolean> features);
 
 }
--- src/main/java/ru/vtb/hackathon/service/atm/AtmServiceImpl.java
+++ src/main/java/ru/vtb/hackathon/service/atm/AtmServiceImpl.java
@@ -9,10 +9,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import ru.vtb.hackathon.entity.AtmEntity;
 import ru.vtb.hackathon.model.RootAtm;
+import ru.vtb.hackathon.model.dto.feature.Feature;
+import ru.vtb.hackathon.model.dto.feature.FeatureActivityState;
+import ru.vtb.hackathon.model.dto.feature.FeatureState;
 import ru.vtb.hackathon.repository.atm.AtmRepository;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -41,5 +46,34 @@ public class AtmServiceImpl implements AtmService {
     @Override
     public List<AtmEntity> findAllAtmAround(Double latitude, Double longitude, Double radius) {
         return repository.findAllWithInRadius(latitude, longitude, radius);
+    }
+
+    @Override
+    public List<AtmEntity> FilterByFeatures(List<AtmEntity> atms, Map<Feature, Boolean> features) {
+        if (features.get(Feature.WHEELCHAIR)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getWheelchair().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.BLIND)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getBlind().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.NFC_FOR_BANK_CARDS)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getNfcForBankCards().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.QR_READ)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getQrRead().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.SUPPORTS_USD)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getSupportsUsd().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.SUPPORTS_CHARGE_RUB)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getSupportsChargeRub().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.SUPPORTS_EUR)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getSupportsEur().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        if (features.get(Feature.SUPPORTS_RUB)) {
+            atms = atms.stream().filter(atm -> atm.getFeatures().getSupportsRub().getFeatureActivity() == FeatureActivityState.AVAILABLE).collect(Collectors.toList());
+        }
+        return atms;
     }
 }
