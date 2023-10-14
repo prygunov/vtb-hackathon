62806ed187ec7d042cefe8b62f6e45d8dfb63b2e
Sedo-KFM <fedor122448@gmail.com>
2023-10-14 11:33:34 +0400

    ATM to Atm naming fix

--- src/main/java/ru/vtb/hackathon/service/atm/ATMService.java
+++ src/main/java/ru/vtb/hackathon/service/atm/ATMService.java
@@ -1,11 +1,11 @@
 package ru.vtb.hackathon.service.atm;
 
-import ru.vtb.hackathon.entity.ATMEntity;
+import ru.vtb.hackathon.entity.AtmEntity;
 
 import java.util.List;
 
 public interface ATMService {
 
-    List<ATMEntity> findAllATMAround(Double latitude, Double longitude, Double radius);
+    List<AtmEntity> findAllATMAround(Double latitude, Double longitude, Double radius);
 
 }
